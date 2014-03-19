5ffc8c18ba1e479ffcbe1ebbe6d1f150f7676022
Andrew Glen-Young <aglenyoung@gmail.com>
2014-03-19 09:40:17 -0400

    Update agy keymap.

--- keyboard/ergodox/keymap_agy.h
+++ keyboard/ergodox/keymap_agy.h
@@ -1,3 +1,5 @@
+#include "action_util.h"
+
 static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Keymap 0: Default Layer
      *
@@ -6,9 +8,9 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
      * | Tab    |   Q  |   W  |   E  |   R  |   T  |  \   |           |  [   |   Y  |   U  |   I  |   O  |   P  | Enter  |
      * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
-     * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
+     * | Fn3    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
      * |--------+------+------+------+------+------|  ~   |           |  ]   |------+------+------+------+------+--------|
-     * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
+     * | Fn4    |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | Fn5    |
      * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
      *   |      |      |      |      |      |                                       |  Lft |  Dn  |  Up  | Rght | RAlt |
      *   `----------------------------------'                                       `----------------------------------'
@@ -19,27 +21,31 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                                 | Space| LAlt |------|       |------| Enter| Space|
      *                                 |      |      | End  |       | PgDn |      |      |
      *                                 `--------------------'       `--------------------'
+     *
+     * FN3: LCtrl or ESC
+     * FN4: LShft or (
+     * FN5: RShft or )
      */
 
     KEYMAP(  // layout: layer 0: default
         // left hand
         ESC, 1,   2,   3,   4,   5,   6,
         TAB, Q,   W,   E,   R,   T,   BSLS,
-        LCTL,A,   S,   D,   F,   G,
-        LSFT,Z,   X,   C,   V,   B,   GRV,
+        FN3, A,   S,   D,   F,   G,
+        FN4, Z,   X,   C,   V,   B,   GRV,
         NO, NO,  NO,  NO,  NO,
                                       DEL, FN1,
                                           HOME,
                                  SPC,LALT, END,
         // right hand
-             7,   8,   9,   0, MINS,EQL,  BSPC,
-             LBRC, Y,   U,   I,   O,   P,   ENT,
-                  H,   J,   K,   L,   SCLN,QUOT,
-             RBRC, N,   M,   COMM,DOT,SLSH,RSFT,
-                       LEFT,UP,  DOWN,RGHT,FN4,
-        FN0, LGUI,
+                7,   8,   9,   0,MINS, EQL,BSPC,
+             LBRC,   Y,   U,   I,   O,   P, ENT,
+                     H,   J,   K,   L,SCLN,QUOT,
+             RBRC,   N,   M,COMM, DOT,SLSH, FN5,
+                       LEFT,DOWN,  UP,RGHT,RALT,
+         FN0,LGUI,
         PGUP,
-        PGDN,ENT, SPC
+        PGDN, ENT, SPC
     ),
 
     /* Keymap 1: Function keys + Teensy (FN7)
@@ -47,13 +53,13 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,--------------------------------------------------.           ,--------------------------------------------------.
      * |        |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |           |  F7  |  F8  |  F9  | F10  | F11  | F12  |        |
      * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
-     * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+     * |        |      |      | PgUp |      |      |      |           |      |      |      |      |      |      |        |
      * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
-     * |        |      |      |      |      |      |------|           |------| Lft  |  Dn  |  Up  | Rght |      |        |
+     * |        |      |      | PgDn |      |      |------|           |------| Lft  |  Dn  |  Up  | Rght |      |        |
      * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
      * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
      * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
-     *   |      |      |      |      |      |                                       |      |      |      |      | FN7  |
+     *   |      |      |      |      |      |                                       |      |      |      |      | FN2  |
      *   `----------------------------------'                                       `----------------------------------'
      *                                        ,-------------.       ,-------------.
      *                                        |      |      |       |      |      |
@@ -62,13 +68,15 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                                 |      |      |------|       |------|      |      |
      *                                 |      |      |      |       |      |      |      |
      *                                 `--------------------'       `--------------------'
+     *
+     * FN2: Teensy
      */
 
     KEYMAP(  // layout: layer 1: F-keys instead of numbers
         // left hand
         TRNS,F1,  F2,  F3,  F4,  F5,  F6,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,PGUP,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,PGDN,TRNS,TRNS,
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,TRNS,TRNS,TRNS,
                                       TRNS,TRNS,
@@ -77,9 +85,9 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // right hand
              F7,  F8,  F9,  F10, F11, F12, TRNS,
              TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-                  LEFT,UP,  DOWN,RGHT,TRNS,TRNS,
+                  LEFT,  UP,DOWN,RGHT,TRNS,TRNS,
              TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-                       TRNS,TRNS,TRNS,TRNS, FN7,
+                       TRNS,TRNS,TRNS,TRNS, FN2,
         TRNS,TRNS,
         TRNS,
         TRNS,TRNS,TRNS
@@ -89,33 +97,77 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* id for user defined functions */
 enum function_id {
     TEENSY_KEY,
+    LSHIFT_LPAREN,
+    RSHIFT_RPAREN,
 };
 
 /*
  * Fn action definition
  */
 static const uint16_t PROGMEM fn_actions[] = {
-    ACTION_DEFAULT_LAYER_SET(0),                    // FN0 - switch to Layer0
-    ACTION_LAYER_MOMENTARY(1),                      // FN1 - push Layer1
-    ACTION_DEFAULT_LAYER_SET(2),                    // FN2 - switch to Layer2
-    ACTION_LAYER_MOMENTARY(3),                      // FN3 - push Layer3
-    ACTION_LAYER_MOMENTARY(4),                      // FN4 - push Layer4
-    ACTION_DEFAULT_LAYER_SET(5),                    // FN5 - switch to Layer5
-    ACTION_LAYER_MOMENTARY(2),                      // FN6 - push Layer2
-    ACTION_FUNCTION(TEENSY_KEY),                    // FN7 - Teensy key
+    [0] = ACTION_DEFAULT_LAYER_SET(0),               // FN0 - switch to Layer0
+    [1] = ACTION_LAYER_MOMENTARY(1),                 // FN1 - push Layer1
+    [2] = ACTION_FUNCTION(TEENSY_KEY),               // FN2 - Teensy key
+    [3] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC),     // FN3 - LCtrl acts as Esc on tap
+    [4] = ACTION_FUNCTION_TAP(LSHIFT_LPAREN),        // FN4 - LShft acts as ( on tap
+    [5] = ACTION_FUNCTION_TAP(RSHIFT_RPAREN),        // FN5 - RShft acts as ) on tap
 };
 
+/*
+ * User defined action function
+ */
 void action_function(keyrecord_t *event, uint8_t id, uint8_t opt)
 {
     print("action_function called\n");
     print("id  = "); phex(id); print("\n");
     print("opt = "); phex(opt); print("\n");
-    if (id == TEENSY_KEY) {
-        clear_keyboard();
-        print("\n\nJump to bootloader... ");
-        _delay_ms(250);
-        bootloader_jump(); // should not return
-        print("not supported.\n");
+
+    keyevent_t evt = event->event;
+    tap_t tap = event->tap;
+
+    switch (id) {
+        case TEENSY_KEY:
+            clear_keyboard();
+            print("\n\nJump to bootloader... ");
+            _delay_ms(250);
+            bootloader_jump(); // should not return
+            print("not supported.\n");
+            break;
+        case LSHIFT_LPAREN:
+            if (evt.pressed) {
+                if (tap.count == 0 || tap.interrupted) {
+                    add_mods(MOD_BIT(KC_LSHIFT));
+                } else {
+                    add_mods(MOD_BIT(KC_LSHIFT));
+                    add_key(KC_9);
+                    send_keyboard_report();
+                    del_mods(MOD_BIT(KC_LSHIFT));
+                    del_key(KC_9);
+                    send_keyboard_report();
+                }
+            } else {
+                if (tap.count == 0 || tap.interrupted) {
+                    del_mods(MOD_BIT(KC_LSHIFT));
+                }
+            }
+            break;
+        case RSHIFT_RPAREN:
+            if (evt.pressed) {
+                if (tap.count == 0 || tap.interrupted) {
+                    add_mods(MOD_BIT(KC_RSHIFT));
+                } else {
+                    add_mods(MOD_BIT(KC_RSHIFT));
+                    add_key(KC_0);
+                    send_keyboard_report();
+                    del_mods(MOD_BIT(KC_RSHIFT));
+                    del_key(KC_0);
+                    send_keyboard_report();
+                }
+            } else {
+                if (tap.count == 0 || tap.interrupted) {
+                    del_mods(MOD_BIT(KC_RSHIFT));
+                }
+            }
+            break;
     }
 }
-
